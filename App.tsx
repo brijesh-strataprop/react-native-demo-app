adb201ac070dbb81a9e9acd602d583daca0f3e3c
Brijesh <kumarbrijesh9021@gmail.com>
2021-12-06 17:51:33 +0530

    added logs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Alert,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -50,16 +51,38 @@ const Section: React.FC<{
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [logs, setLogs] = React.useState([]);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   React.useEffect(() => {
-    codePush.sync({
-      updateDialog: true,
-      installMode: codePush.InstallMode.IMMEDIATE,
-    });
+    codePush
+      .checkForUpdate()
+      .then(update => {
+        if (update) {
+          setLogs([...logs, 'Started at' + new Date().getTime()]);
+          codePush.sync(
+            {
+              updateDialog: true,
+              installMode: codePush.InstallMode.IMMEDIATE,
+              mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+            },
+            status => {
+              for (let key in codePush.SyncStatus) {
+                if (status === codePush.SyncStatus[key]) {
+                  setLogs([...logs, key.replace(/_/g, '')]);
+                  break;
+                }
+              }
+            },
+          );
+        }
+      })
+      .catch(error => {
+        Alert.alert('Error: app update', error.message);
+      });
   }, []);
 
   return (
@@ -73,6 +96,9 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="Logs">
+            <Text>{JSON.stringify(logs)}</Text>
+          </Section>
           <Section title="Number One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
